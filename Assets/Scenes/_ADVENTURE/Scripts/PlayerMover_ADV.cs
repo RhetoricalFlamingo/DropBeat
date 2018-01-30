b1c51d1c0ef284fd6f060538050b19e3b8521125
MC <mcmailbox014@gmail.com>
2018-01-30 12:01:35 -0500

    Test
    Rudimentary Adventure Movement • Basic Beat Clock • Placeholder Sprites

---
+++ Assets/Scenes/_ADVENTURE/Scripts/PlayerMover_ADV.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover_ADV : MonoBehaviour {
+
+	float playerSpeed = 0;
+	public float playerSpeedSet = 0;
+
+	Vector3 previousPosition = new Vector2 (0, 0);
+	Vector3 newPosition = new Vector2 (0, 0);
+	private float startTime;
+	private float journeyDistance;
+
+	public float moveSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		journeyDistance = Vector2.Distance (previousPosition, newPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerSpeed = playerSpeedSet;
+		playerSpeed *= Time.deltaTime;
+
+
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) || Input.GetAxis ("rJoystickX") < -.85f) {
+			//transform.position += -Vector3.right * playerSpeed;
+			startTime = Time.time;
+			previousPosition = transform.position;
+			newPosition = transform.position -= Vector3.right * 1.5f;
+			float distCovered = (Time.time - startTime) * moveSpeed;
+			float fracJourney = distCovered / journeyDistance;
+			transform.position = Vector3.Lerp (previousPosition, newPosition, fracJourney);
+		}
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow) || Input.GetAxis ("rJoystickX") > .85f) {
+			//transform.position += Vector3.right * playerSpeed;
+			startTime = Time.time;
+			previousPosition = transform.position;
+			newPosition = transform.position += Vector3.right * 1.5f;
+			float distCovered = (Time.time - startTime) * moveSpeed;
+			float fracJourney = distCovered / journeyDistance;
+			transform.position = Vector3.Lerp (previousPosition, newPosition, fracJourney);
+		}
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow) || Input.GetAxis ("rJoystickY") > .85f) {
+			//transform.position += Vector3.up * playerSpeed;
+			startTime = Time.time;
+			previousPosition = transform.position;
+			newPosition = transform.position += Vector3.up * 1.5f;
+			float distCovered = (Time.time - startTime) * moveSpeed;
+			float fracJourney = distCovered / journeyDistance;
+			transform.position = Vector3.Lerp (previousPosition, newPosition, fracJourney);
+		}
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow) || Input.GetAxis ("rJoystickY") < -.85f) {
+			//transform.position += -Vector3.up * playerSpeed;
+			startTime = Time.time;
+			previousPosition = transform.position;
+			newPosition = transform.position -= Vector3.up * 1.5f;
+			float distCovered = (Time.time - startTime) * moveSpeed;
+			float fracJourney = distCovered / journeyDistance;
+			transform.position = Vector3.Lerp (previousPosition, newPosition, fracJourney);
+		}
+	
+		transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
+
+		Debug.Log ("X: " + Input.GetAxis ("rJoystickX"));
+		Debug.Log ("Y: " + Input.GetAxis ("rJoystickY"));
+	}
+}
---
+++ Assets/Scenes/_ADVENTURE/Scripts/cameraControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject textParent;
+	//public Text currentText;
+
+	bool INCOMBAT = false;
+	bool INADVENTURE = true;
+	Vector3 combatCam = new Vector3 (0f, 0f, 0f);
+	Vector3 adventureCam = new Vector3 (0f, 0f, 0f);	//make getcomponent of player transform
+
+	int intCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (INCOMBAT) {
+			transform.position = combatCam;
+		}
+		if (INADVENTURE) {
+			adventureCam = player.transform.position - new Vector3 (0f, 0f, 10f);
+			transform.position = adventureCam;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			intCounter++;
+		}
+
+		if (intCounter > 0 && intCounter < 100) {
+			textParent.SetActive (true);
+
+			if (intCounter == 1) {
+				//currentText.text = "Hey! You!\n\n(SPACE)";
+			}
+			if (intCounter == 2) {
+				//currentText.text = ".....\n\n(SPACE)";
+			}
+			if (intCounter == 3) {
+				//currentText.text = "Let's Fight!\n\n(SPACE)"; 
+			}
+		}
+	}
+}

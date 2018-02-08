db05423b41a2de639165fdf54c05c94b6b43d9e7
MC <mcmailbox014@gmail.com>
2018-02-08 10:38:37 -0500

    State Machine Cont.
    Initialized character stats; working through (turnFinished) execution of actions

--- Assets/Scenes/_BATTLE/Scripts/stateMachine.cs
+++ Assets/Scenes/_BATTLE/Scripts/stateMachine.cs
@@ -44,7 +44,7 @@ public class stateMachine : MonoBehaviour {
 		public string item3;
 
 		public int fightDam;
-
+		public bool charged;
 	}
 
 	CharacterData[] charDatas = new CharacterData[6];
@@ -55,7 +55,43 @@ public class stateMachine : MonoBehaviour {
 
 		faceButtonRend = faceButtonUI.GetComponent<SpriteRenderer> ();
 
-		//Finish character class declaration and initialisation
+//**************STATS INIT***************************************************
+//-1
+		charDatas [0].maxHp = 100;
+		charDatas [0].currentHp = charDatas [0].maxHp;
+		charDatas [0].isAlive = true;
+		//charDatas [0].dance1 = Headspin (AOE)
+		//charDatas [0].dance2 = Beatdown (Heavy, single target, recoil)
+		//charDatas [0].dance3 = Upstage  (Next AI to go loses their turn)
+		//charDatas [0].item1 = Pick-Me-Up (heal)
+		//charDatas [0].item2 = Metal Amp (enemies and allies both deal 2X Dam this turn)
+		//charDatas [0].item3 = _empty_
+		charDatas [0].fightDam = 18;
+		charDatas [0].charged = false;
+//-2
+		charDatas [1].maxHp = 85;
+		charDatas [1].currentHp = charDatas [0].maxHp;
+		charDatas [1].isAlive = true;
+		//charDatas [0].dance1 = All Together Now (group heal)
+		//charDatas [0].dance2 = Fire Bird (damage and life-drain)
+		//charDatas [0].dance3 = High Kick (heavy single target damage, has cooldown)
+		//charDatas [0].item1 = Pick-Me-Up (heal)
+		//charDatas [0].item2 = Metal Amp (enemies and allies both deal 2X Dam this turn)
+		//charDatas [0].item3 = _empty_
+		charDatas [1].fightDam = 10;
+		charDatas [1].charged = false;
+//-3
+		charDatas [2].maxHp = 120;
+		charDatas [2].currentHp = charDatas [0].maxHp;
+		charDatas [2].isAlive = true;
+		//charDatas [0].dance1 = Lawnmower (self heal)
+		//charDatas [0].dance2 = Windmill (Reflect damage this turn)
+		//charDatas [0].dance3 = Sprinkler (taunt)
+		//charDatas [0].item1 = Pick-Me-Up (heal)
+		//charDatas [0].item2 = Metal Amp (enemies and allies both deal 2X Dam this turn)
+		//charDatas [0].item3 = _empty_
+		charDatas [2].fightDam = 12;
+		charDatas [2].charged = false;
 	}
 
 	void Update () {
@@ -170,14 +206,38 @@ public class stateMachine : MonoBehaviour {
 		if (currentTurn >= 4) {
 			for (int i = 4; i < 7; i++) {
 
-				decisionAI = (int)Random.Range(0, 3);
+				decisionAI = (int)Random.Range(0, 99);
+
+				if (charDatas [i].charged) {	//If the last turn was spent charging up, this turn is a guaranteed FIGHT action
+					decisionAI = 0;
+				}
+				if (decisionAI > 40 && decisionAI <= 60 && charDatas [i].currentHp <= (int)(charDatas [i].maxHp * .3f)) {	 //If BLAST would kill the AI, it uses FIGHT instead
+					decisionAI = 0;
+				}
 
 //**************Fight
-				if (decisionAI == 0) {
-					charDatas[(int)Random.Range (0, 2)].currentHp -= charDatas [i].fightDam;
+				if (decisionAI <= 40) {
+					if (charDatas [i].charged) {
+						charDatas [(int)Random.Range (0, 2)].currentHp -= (int)(charDatas [i].fightDam * 2.5);
+						charDatas [i].charged = false;
+					} else if (!charDatas [i].charged) {
+						charDatas [(int)Random.Range (0, 2)].currentHp -= charDatas [i].fightDam;
+					}
 				}
-				if (decisionAI == 1) {
-					
+//**************Blast
+				else if (decisionAI > 40 && decisionAI <= 60) {
+					charDatas [0].currentHp -= (int)(charDatas [i].fightDam * .4f);
+					charDatas [1].currentHp -= (int)(charDatas [i].fightDam * .4f);
+					charDatas [2].currentHp -= (int)(charDatas [i].fightDam * .4f);
+					charDatas [i].currentHp -= (int)(charDatas [i].maxHp * .3f);
+				}
+//**************Legion
+				else if (decisionAI > 60 && decisionAI <= 70) {
+					charDatas [(int)Random.Range (3, 5)].currentHp += 20;
+				}
+//**************Charge
+				else if (decisionAI > 70) {
+					charDatas [i].charged = true;
 				}
 
 
@@ -192,15 +252,7 @@ public class stateMachine : MonoBehaviour {
 
 ///////////////////////////////////////////////////////////////FIGHT
 			if (currentAction == "fight") {
-				if (target == 1) {
-					//deal fight damage to target 1
-				}
-				if (target == 2) {
-					//deal fight damage to target 2
-				}
-				if (target == 3) {
-					//deal fight damage to target 3
-				}
+				charDatas [target + 2].currentHp -= charDatas [currentTurn].fightDam;
 			}
 ///////////////////////////////////////////////////////////////DANCE
 			if (currentTurn == 1) {			//PC1
@@ -335,17 +387,6 @@ public class stateMachine : MonoBehaviour {
 						//PC 1's item 2 on target 3
 					}
 				}
-				if (currentAction == "item3") {
-					if (target == 1) {
-						//PC 1's item 3 on target 1
-					}
-					if (target == 2) {
-						//PC 1's item 3 on target 2
-					}
-					if (target == 3) {
-						//PC 1's item 3 on target 3
-					}
-				}
 			}
 ///////////////////////////////////////////////////////////////
 			if (currentTurn == 2) {			//PC2
@@ -371,17 +412,6 @@ public class stateMachine : MonoBehaviour {
 						//PC 2's item 2 on target 3
 					}
 				}
-				if (currentAction == "item3") {
-					if (target == 1) {
-						//PC 2's item 3 on target 1
-					}
-					if (target == 2) {
-						//PC 2's item 3 on target 2
-					}
-					if (target == 3) {
-						//PC 2's item 3 on target 3
-					}
-				}
 			}
 ///////////////////////////////////////////////////////////////
 			if (currentTurn == 3) {			//PC23
@@ -405,17 +435,6 @@ public class stateMachine : MonoBehaviour {
 					}
 					if (target == 3) {
 						//PC 3's item 2 on target 3
-					}
-				}
-				if (currentAction == "item3") {
-					if (target == 1) {
-						//PC 3's item 3 on target 1
-					}
-					if (target == 2) {
-						//PC 3's item 3 on target 2
-					}
-					if (target == 3) {
-						//PC 3's item 3 on target 3
 					}
 				}
 			}

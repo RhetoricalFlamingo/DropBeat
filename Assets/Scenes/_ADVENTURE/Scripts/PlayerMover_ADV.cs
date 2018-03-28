a9e4f880d2444ca11af30b7c3ffa8901fb61ab98
MC <mcmailbox014@gmail.com>
2018-03-28 12:57:53 -0400

    PlayerMover Indicates Proximity to chatterNPCs
    When player is near chatterNPCs, boolean turns true (will be used for conversation system)

--- Assets/Scenes/_ADVENTURE/Scripts/PlayerMover_ADV.cs
+++ Assets/Scenes/_ADVENTURE/Scripts/PlayerMover_ADV.cs
@@ -16,6 +16,17 @@ public class PlayerMover_ADV : MonoBehaviour {
 	public GameObject ABTran;
 	bool inBattle = false;
 
+	public GameObject babot;
+	public bool nearBabot;
+	public GameObject npc1;
+	public bool nearNpc1;
+	public GameObject npc2;
+	public bool nearNpc2;
+	public GameObject npc3;
+	public bool nearNpc3;
+
+	float distanceCheck = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 	}
@@ -32,22 +43,22 @@ public class PlayerMover_ADV : MonoBehaviour {
 
 		if (!inBattle) {
 
-			if ((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) || Input.GetAxis ("rJoystickX") < -.85f) && rStickNeutral) {
+			if ((Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetAxis ("rJoystickX") < -.85f) && rStickNeutral) {
 				newPosition = transform.position -= Vector3.right * 1.5f;
 				transform.position = Vector2.MoveTowards (transform.position, newPosition, moveSpeed);
 				rStickNeutral = false;
 			}
-			if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow) || Input.GetAxis ("rJoystickX") > .85f) && rStickNeutral)  {
+			if ((Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow) || Input.GetAxis ("rJoystickX") > .85f) && rStickNeutral)  {
 				newPosition = transform.position += Vector3.right * 1.5f;
 				transform.position = Vector2.MoveTowards (transform.position, newPosition, moveSpeed);
 				rStickNeutral = false;
 			}
-			if ((Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow) || Input.GetAxis ("rJoystickY") > .85f) && rStickNeutral) {
+			if ((Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetAxis ("rJoystickY") > .85f) && rStickNeutral) {
 				newPosition = transform.position += Vector3.up * 1.5f;
 				transform.position = Vector2.MoveTowards (transform.position, newPosition, moveSpeed);
 				rStickNeutral = false;
 			}
-			if ((Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow) || Input.GetAxis ("rJoystickY") < -.85f) && rStickNeutral) {
+			if ((Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetAxis ("rJoystickY") < -.85f) && rStickNeutral) {
 				newPosition = transform.position -= Vector3.up * 1.5f;
 				transform.position = Vector2.MoveTowards (transform.position, newPosition, moveSpeed);
 				rStickNeutral = false;
@@ -58,6 +69,29 @@ public class PlayerMover_ADV : MonoBehaviour {
 //			}
 	
 			transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
+			chatterCheck ();
 		}
+	}
+
+	void chatterCheck ()	{
+		if (Vector2.Distance (gameObject.transform.position, babot.transform.position) <= distanceCheck) {
+			nearBabot = true;
+		} else
+			nearBabot = false;
+		
+		if (Vector2.Distance (gameObject.transform.position, npc1.transform.position) <= distanceCheck) {
+			nearNpc1 = true;
+		} else
+			nearNpc1 = false;
+		
+		if (Vector2.Distance (gameObject.transform.position, npc2.transform.position) <= distanceCheck) {
+			nearNpc2 = true;
+		} else
+			nearNpc2 = false;
+		
+		if (Vector2.Distance (gameObject.transform.position, npc3.transform.position) <= distanceCheck) {
+			nearNpc3 = true;
+		} else
+			nearNpc3 = false;
 	}
 }

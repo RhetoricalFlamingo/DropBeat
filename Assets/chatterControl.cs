0cd65e486b6b4e0e7801e4861780dee286e2aa72
MC <mcmailbox014@gmail.com>
2018-03-28 12:41:02 -0400

    AV Setup
    Some art and music imported, gameobjects created, and scripts made: none of this has been implemented though

---
+++ Assets/chatterControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chatterControl : MonoBehaviour {
+	int babotchatterCount = 0;
+	string babot_1 = "HEY! YOU!";
+	string babot_2 = "Don't you know who we are??";
+	string babot_3 = "......You don't?";
+	string babot_4 = ".....................";
+	string babot_5 = "...WE are the Babots, the raddest dancemeisters in the YARD!";
+	string babot_6 = "And if you don't give us room to dance, we'll SHUT YOU DOWN.";
+	string babot_7 = "Go talk to the wallflowers or something; don't bother us again.";
+	string babot_8 = "Alright, you asked for it...BABOTS: G E T  F U N K Y!";
+
+	int npc1chatterCount = 0;
+	string npc11 = "Those dang Babots ain't got no rhythm...";
+	string npc12 = "Try only pressing buttons on the beat to get an edge!";
+	string npc13 = "You'll know if you got it cause you'll see the word 'FLAWLESS'";
+
+	int npc2chatterCount = 0;
+	string npc21 = "Go with the flow, don't worry about the music";
+	string npc22 = "You have as long as you want to take your turn!";
+
+	int npc3chatterCount = 0;
+	string npc31 = "Did you know Babots pick their moves almost completely at random?";
+	string npc32 = "That's right! They're complete idiots!";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/musicController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicController : MonoBehaviour {
+
+	public AudioSource BGM;
+	public AudioSource SFX;
+
+	public AudioClip adventureLoop_BGM;
+	public AudioClip troublemakers_BGM;
+	public AudioClip battleIntro_BGM;
+	public AudioClip battle_BGM;
+	public AudioClip victoryIntro_BGM;
+	public AudioClip victory_BGM;
+	public AudioClip chatter;
+
+	public GameObject Transition;
+
+	bool inBattle;
+	bool battleIntroPlayed;
+	bool toggleChange = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		inBattle = Transition.GetComponent<ABTransition> ().inBattle;
+
+		if (inBattle) {
+			BGM.clip = battleIntro_BGM;
+		}
+
+		if (toggleChange) {
+			BGM.Play ();
+			toggleChange = false;
+		}
+	}
+}
